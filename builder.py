e66a698f5deb5120ceea98fa7a7bfd73e58b41c3
Matheus Degiovani <opensource@matheusd.com>
2018-09-17 10:51:08 -0300

    Adding downgrade warning

--- builder.py
+++ builder.py
@@ -20,6 +20,17 @@ README_TEMPLATE = """
 These are binaries for the decred project built from unstable, under development
 code. Do not use in production. You will not receive support for them.
 
+**MIGHT NOT ALLOW DOWNGRADE**
+
+Sometimes dcrd/dcrwallet changes include migrations which cannot be downgraded.
+Since these builds are made from code that is under development (usually from
+the master branch of the respective repos), this means that they may include such
+migrations. Therefore after running these builds, you might be unable to go back
+to older versions of the software without deleting the blockchain or wallet
+files.
+
+**YOU HAVE BEEN WARNED**
+
 [Latest Release](https://github.com/{BUILD_REPO_OWNER}/{BUILD_REPO_REPO}/releases/latest)
 
 ## Version Information
